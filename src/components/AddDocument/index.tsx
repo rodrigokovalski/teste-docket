e65dd34eb329eb4522e006147159c2e8d45d7de2
Rodrigo Kovalski <rodrigokvk@gmail.com>
2022-02-19 21:53:14 -0300

    Criando layout dos documentos criados

--- src/components/AddDocument/index.tsx
+++ src/components/AddDocument/index.tsx
@@ -7,55 +7,55 @@ export function AddDocument(){
             <div className={styles.formContent}>
                 <form action="">
                 <div className={styles.formGroup}>
-                        <label htmlFor="doc-name">Nome do Documento: *</label>
-                        <input type="text" id="doc-name" className={styles.formControl}/>
+                        <label htmlFor="doc-name">Nome do Documento: <span>*</span></label>
+                        <input type="text" id="doc-name" className={styles.formControl} placeholder="Digite aqui" required/>
                     </div>
                     <div className={styles.formGroup}>
-                        <label htmlFor="person-type">Tipo de pessoa: *</label>
+                        <label htmlFor="person-type">Tipo de pessoa: <span>*</span></label>
                         <select name="person-type" id="person-type" className={styles.formControl}>
                             <option value="1">Pessoa Física</option>
                             <option value="2">Pessoa Jurídica</option>
                         </select>
                     </div>
                     <div className={styles.formGroup}>
-                        <label htmlFor="cpf">CPF: *</label>
-                        <input type="text" id="cpf" className={styles.formControl} />
+                        <label htmlFor="cpf">CPF: <span>*</span></label>
+                        <input type="tel" id="cpf" className={styles.formControl} placeholder="Digite aqui" required />
                     </div>
                     <div className={styles.formGroup}>
-                        <label htmlFor="name">Nome Completo: *</label>
-                        <input type="text" id="name" className={styles.formControl}/>
+                        <label htmlFor="name">Nome Completo: <span>*</span></label>
+                        <input type="text" id="name" className={styles.formControl} placeholder="Digite aqui" required/>
                     </div>
                     <h5>Dados do cartório</h5>
                     <div className={styles.formGroup}>
-                        <label htmlFor="cep">CEP: *</label>
-                        <input type="text" id="cep" className={styles.formControl} />
+                        <label htmlFor="cep">CEP: <span>*</span></label>
+                        <input type="tel" id="cep" className={styles.formControl} placeholder="Digite aqui" required />
                     </div>
                     <div className={styles.formGroup}>
                         <div className={styles.twoFields}>
                             <div className={styles.left}>
-                                <label htmlFor="street">Rua: *</label>
-                                <input type="text" id="street" className={styles.formControl} />
+                                <label htmlFor="street">Rua: <span>*</span></label>
+                                <input type="text" id="street" className={styles.formControl} placeholder="Digite aqui" required />
                             </div>
                             <div className={styles.right}>
-                                <label htmlFor="number">Número: *</label>
-                                <input type="text" id="number" className={styles.formControl} />
+                                <label htmlFor="number">Número: <span>*</span></label>
+                                <input type="tel" id="number" className={styles.formControl} placeholder="Digite aqui" required />
                             </div>
                         </div>
                     </div>
                     <div className={styles.formGroup}>
                         <div className={styles.twoFields}>
                             <div className={styles.left}>
-                                <label htmlFor="city">Cidade: *</label>
-                                <input type="text" id="city" className={styles.formControl} />
+                                <label htmlFor="city">Cidade: <span>*</span></label>
+                                <input type="text" id="city" className={styles.formControl} placeholder="Digite aqui" required />
                             </div>
                             <div className={styles.right}>
-                                <label htmlFor="uf">UF: *</label>
-                                <input type="text" id="uf" className={styles.formControl} />
+                                <label htmlFor="uf">UF: <span>*</span></label>
+                                <input type="text" id="uf" className={styles.formControl} placeholder="Digite aqui" required />
                             </div>
                         </div>
                     </div>
                     <div className={styles.formGroup}>
-                        <button type="button">
+                        <button type="submit">
                             Criar documento
                         </button>
                     </div>
--- src/components/DocsRequested/index.tsx
+++ src/components/DocsRequested/index.tsx
@@ -1,14 +1,44 @@
 import styles from './styles.module.scss'
 
 import DocIcon from '../../assets/doc-icon.svg'
+import RemoveIcon from '../../assets/remove-icon.svg'
 
 export function DocsRequested() {
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.noDocument}>
-                <img src={DocIcon} alt="Nenhum documento criado" />
-                <p>Nenhum documento criado</p>
+        <>
+            {/* <div className={styles.wrapper}>
+                <div className={styles.noDocument}>
+                    <img src={DocIcon} alt="Nenhum documento criado" />
+                    <p>Nenhum documento criado</p>
+                </div>
+            </div> */}
+            <div className={styles.docsRequested}>
+                <h4>1 documento solicitado</h4>
+                <div className={styles.document}>
+                    <div className={styles.header}>
+                        <h5>Certidão de nascimento</h5>
+                        <button type="button" className={styles.removeIcon}>
+                            <img src={RemoveIcon} alt="" />
+                        </button>
+                    </div>
+                    <div className={styles.data}>
+                        <div className={styles.personal}>
+                            <h6><strong>Pessoa Física</strong></h6>
+                            <p>Nome: João da Silva Santos</p>
+                            <p>CPF: 124.154.156-45</p>
+                        </div>
+                        <div className={styles.registry}>
+                            <h6><strong>Dados do cartório</strong></h6>
+                            <p>CEP: 999999-999</p>
+                            <p>Rua: Eugênio de Medeiros</p>
+                            <p>Cidade: São Paulo</p>
+                        </div>
+                        <div className={styles.createdAt}>
+                            <p><strong>Data de criação:</strong> 11 de maio de 2021</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
